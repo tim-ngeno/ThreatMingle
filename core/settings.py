006308b6b58adda1e76054604dadb725e310272b
Tim Ngeno <timngeno424@gmail.com>
2023-12-16 08:28:39 +0300

    update hosts

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default=get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOST = ['localhost', '127.0.0.1']
+ALLOWED_HOST = ['localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
