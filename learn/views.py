d49f217e84a2e9ee1037610837e94e743418e2ea
Tim Ngeno <timngeno424@gmail.com>
2023-12-22 23:07:39 +0300

    update learning content modules with data

--- learn/views.py
+++ learn/views.py
@@ -9,24 +9,46 @@ def learning_view(request):
     """
     data = [
         {
-            'title': 'Intro to Cyber Security',
-            'content': 'Lorem Ipsum dolor sit amet...',
+            'title': 'Introduction to Cybersecurity',
+            'content': 'This module provides an introduction to the \
+            field of cybersecurity, covering its importance, key \
+            concepts, and fundamental principles.',
         },
         {
-            'title': 'Intro to Network Security',
-            'content': 'Lorem Ipsum dolor sit amet...',
+            'title': 'Network Security Basics',
+            'content': 'Explore the foundations of network security, including concepts like firewalls, VPNs, and secure communication protocols.',
         },
         {
-            'title': 'What is DDOS',
-            'content': 'Lorem Ipsum dolor sit amet...',
+            'title': 'Web Application Security',
+            'content': 'Learn about common web application security vulnerabilities and best practices for securing web applications against attacks.',
         },
         {
-            'title': 'Is Cyber Security really all that?',
-            'content': 'Lorem Ipsum dolor sit amet...',
+            'title': 'Cryptography Essentials',
+            'content': 'An overview of cryptographic techniques and their applications in securing information and communication.',
         },
         {
-            'title': 'Keeping yourself safe from cyber threats',
-            'content': 'Lorem Ipsum dolor sit amet...',
+            'title': 'Malware Analysis Techniques',
+            'content': 'Dive into the world of malware analysis, understanding different types of malware and techniques for analyzing malicious code.',
+        },
+        {
+            'title': 'Incident Response Strategies',
+            'content': 'Explore strategies and best practices for incident response, including detection, analysis, containment, eradication, and recovery.',
+        },
+        {
+            'title': 'Cloud Security Fundamentals',
+            'content': 'Understand the unique challenges and solutions related to securing cloud environments, covering cloud architecture, identity management, and data protection.',
+        },
+        {
+            'title': 'Security Awareness Training',
+            'content': 'Develop an understanding of the human element in cybersecurity, focusing on security awareness training and promoting a security-conscious culture.',
+        },
+        {
+            'title': 'Mobile Security Principles',
+            'content': 'Examine security principles and challenges associated with mobile devices, mobile applications, and the protection of sensitive data on mobile platforms.',
+        },
+        {
+            'title': 'IoT Security Considerations',
+            'content': 'Investigate security considerations and best practices for securing Internet of Things (IoT) devices and ecosystems.',
         },
     ]
 
