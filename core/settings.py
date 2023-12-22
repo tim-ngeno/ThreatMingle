98320d43f670683909673d20a50a9b9f5163bd5d
Tim Ngeno <timngeno424@gmail.com>
2023-12-22 15:03:11 +0300

    update hosts and Debug value

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default=get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOST = ['*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
